766ed53e0de0c9eb114396ae2264039c964b3b3d
Steve Wakhungu <stwakhungu@gmail.com>
2022-06-17 23:58:02 +0300

    commit tree 6bb6fd1691e3e04738d2c88e71195047df755301

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));  
   
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  var main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
+    var h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
@@ -15,3 +18,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+renderBooks(books)
